c0c9195357f422ebacbda4bd709cf427b05116f0
bdsilver89 <github@briansilver.net>
2019-11-15 20:41:07 -0500

    Fixed Cmake build for c++ googltests

--- tests/test_musher_cpp.cpp
+++ tests/test_musher_cpp.cpp
@@ -5,17 +5,6 @@
 #include "functional_test.h"
 
 
-// int main()
-// {
-//     std::cout << "================================================================" << std::endl;
-//     std::cout << "This is the c++ wrapper to test the Musher library"               << std::endl;
-//     std::cout << "================================================================" << std::endl;
-
-//     CPrintFunctionalMessage("This is a message for a functional program");
-
-//     return 0;
-// }
-
 // IndependentMethod is a test case - here, we have 2 tests for this 1 test case
 TEST(PrintFunctionalMessage, PrintsMessage) {
 	CPrintFunctionalMessage("This is a message for a functional program");

9ff198e91d8c464372ca221e79b124a121f29d7f
bdsilver89 <github@briansilver.net>
2019-11-28 11:54:22 -0500

    Fix header file ifndef wrapper for python_utils

--- include/python_utils.h
+++ include/python_utils.h
@@ -1,6 +1,11 @@
+#ifndef __PYTHON_UTILS__
+#define __PYTHON_UTILS__
+
 #include <Python.h> // Must be first
 #include <vector>
 
 PyObject* vector_to_list_float(const std::vector<float> &data);
 PyObject* vector_to_list_int(const std::vector<int> &data);
 PyObject* vector_to_list_uint8_t(const std::vector<uint8_t> &data);
+
+#endif /* __PYTHON_UTILS__ */

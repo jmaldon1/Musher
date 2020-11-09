253d89b19dcf852859ff25aca6e71f2c5909340d
Joshua Maldonado <joshjm9915@gmail.com>
2020-11-08 23:59:25 -0500

    moved doxygen to header

--- src/core/key.cpp
+++ src/core/key.cpp
@@ -7,46 +7,7 @@
 namespace musher {
 namespace core {
 
-/**
- * @brief Select a key profile given the type.
- *
- * About the Key Profiles:
- * - 'Diatonic' - binary profile with diatonic notes of both modes. Could be useful for ambient music or diatonic music
- *      which is not strictly 'tonal functional'
- * - 'Tonic Triad' - just the notes of the major and minor chords. Exclusively for testing.
- * - 'Krumhansl' - reference key profiles after cognitive experiments with users. They should work generally fine for
- *      pop music.
- * - 'Temperley' - key profiles extracted from corpus analysis of euroclassical music. Therefore, they perform best on
- *      this repertoire (especially in minor).
- * - 'Shaath' -  profiles based on Krumhansl's specifically tuned to popular and electronic music.
- * - 'Noland' - profiles from Bach's 'Well Tempered Klavier'.
- * - 'edma' - automatic profiles extracted from corpus analysis of electronic dance music [3]. They normally perform
- *      better that Shaath's
- * - 'edmm' - automatic profiles extracted from corpus analysis of electronic dance music and manually tweaked according
- *      to heuristic observation. It will report major modes (which are poorly represented in EDM) as minor, but improve
- *      performance otherwise [3].
- * - 'braw' - profiles obtained by calculating the median profile for each mode from a subset of BeatPort dataset. There
- *      is an extra profile obtained from ambiguous tracks that are reported as minor[4]
- * - 'bgate' - same as braw but zeroing the 4 less relevant elements of each profile[4]
- *
- * References:
- *  [1] E. Gómez, "Tonal Description of Polyphonic Audio for Music Content
- *  Processing," INFORMS Journal on Computing, vol. 18, no. 3, pp. 294–304, 2006.
- *  [2] D. Temperley, "What's key for key? The Krumhansl-Schmuckler key-finding algorithm reconsidered", Music
- *  Perception vol. 17, no. 1,  pp. 65-100, 1999.
- *  [3] Á. Faraldo, E. Gómez, S. Jordà, P.Herrera, "Key Estimation in Electronic"
- *  Dance Music. Proceedings of the 38th International Conference on information"
- *  Retrieval, pp. 335-347, 2016.
- *  [4] Faraldo, Á., Jordà, S., & Herrera, P. (2017, June). A multi-profile method"
- *  for key estimation in edm. In Audio Engineering Society Conference: 2017 AES"
- *  International Conference on Semantic Audio. Audio Engineering Society.
- *
- * --essentia: https://github.com/MTG/essentia/blob/master/src/algorithms/tonal/key.cpp
- *
- * @param profile_type Key profile type.
- * @return std::vector<std::vector<double>> Key profile
- */
-std::vector<std::vector<double>> SelectKeyProfile(const PolyphicProfile profile_type) {
+std::vector<std::vector<double>> SelectKeyProfile(const std::string profile_type) {
   const std::vector<std::vector<double>> diatonic{
     { 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1 },
     { 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1 },
@@ -123,59 +84,23 @@ std::vector<std::vector<double>> SelectKeyProfile(const PolyphicProfile profile_
     { 1.00, 0.26, 0.35, 0.29, 0.44, 0.36, 0.21, 0.78, 0.26, 0.25, 0.32, 0.26 },
   };
 
-  switch (profile_type) {
-    case PolyphicProfile::Diatonic:
-      return diatonic;
-    case PolyphicProfile::Krumhansl:
-      return krumhansl;
-    case PolyphicProfile::Temperley:
-      return temperley;
-    case PolyphicProfile::Weichai:
-      return weichai;
-    case PolyphicProfile::Tonictriad:
-      return tonictriad;
-    case PolyphicProfile::Temperley2005:
-      return temperley2005;
-    case PolyphicProfile::Thpcp:
-      return thpcp;
-    case PolyphicProfile::Shaath:
-      return shaath;
-    case PolyphicProfile::Gomez:
-      return gomez;
-    case PolyphicProfile::Noland:
-      return noland;
-    case PolyphicProfile::Edmm:
-      return edmm;
-    // -- Profiles with other --
-    case PolyphicProfile::Bgate:
-      return bgate;
-    case PolyphicProfile::Braw:
-      return braw;
-    case PolyphicProfile::Edma:
-      return edma;
-    default:
-      throw std::runtime_error("SelectKeyProfile: Was a profile type not accounted for?");
-  }
+  if(profile_type == "Diatonic") return diatonic;
+  else if (profile_type == "Krumhansl") return krumhansl;
+  else if (profile_type == "Temperley") return temperley;
+  else if (profile_type == "Weichai") return weichai;
+  else if (profile_type == "Tonictriad") return tonictriad;
+  else if (profile_type == "Temperley2005") return temperley2005;
+  else if (profile_type == "Thpcp") return thpcp;
+  else if (profile_type == "Shaath") return shaath;
+  else if (profile_type == "Gomez") return gomez;
+  else if (profile_type == "Noland") return noland;
+  else if (profile_type == "Edmm") return edmm;
+  else if (profile_type == "Bgate") return bgate;
+  else if (profile_type == "Braw") return braw;
+  else if (profile_type == "Edma") return edma;
+  else throw std::runtime_error("SelectKeyProfile: Was a profile type not accounted for?");
 }
 
-/**
- * @brief Add contribution harmonics to chords.
- * Each note contribute to the different harmonics:
- * 1.- first  harmonic  f   -> i
- * 2.- second harmonic  2*f -> i
- * 3.- third  harmonic  3*f -> i+7
- * 4.- fourth harmonic  4*f -> i
- * ..
- * The contribution is weighted depending of the slope
- *
- * @param chords Chords
- * @param pitch_class pitch class
- * @param contribution harmonic contribution
- * @param num_harmonics Number of harmonics that should contribute to the polyphonic profile (1 only considers the
- * fundamental harmonic).
- * @param slope Value of the slope of the exponential harmonic contribution to the polyphonic profile.
- * @return std::vector<double> chords with added contribution harmonics
- */
 std::vector<double> AddContributionHarmonics(const std::vector<double>& chords,
                                              const int pitch_class,
                                              const double contribution,
@@ -209,21 +134,6 @@ std::vector<double> AddContributionHarmonics(const std::vector<double>& chords,
   return chords_with_harmonics;
 }
 
-/**
- * @brief Adds the contribution of a chord with root note 'root' to its major triad.
- * A major triad includes notes from three different classes of pitch: the root, the major 3rd and perfect 5th.
- * This is the most relaxed, most consonant chord in all of harmony.
- * @see http://www.songtrellis.com/directory/1146/chordTypes/majorChordTypes/majorTriad
- * The three notes of the chord have the same weight
- *
- * @param chords Chords
- * @param root root note
- * @param contribution harmonic contribution
- * @param num_harmonics Number of harmonics that should contribute to the polyphonic profile (1 only considers the
- * fundamental harmonic).
- * @param slope Value of the slope of the exponential harmonic contribution to the polyphonic profile.
- * @return std::vector<double> Chords with contribution added to its major triad.
- */
 std::vector<double> AddMajorTriad(const std::vector<double>& chords,
                                   const int root,
                                   const double contribution,
@@ -244,20 +154,6 @@ std::vector<double> AddMajorTriad(const std::vector<double>& chords,
   return AddContributionHarmonics(chords_third, fifth, contribution, num_harmonics, slope);
 }
 
-/**
- * @brief Adds the contribution of a chord with root note 'root' to its minor triad
- * A minor triad includes notes from three different classes of pitch: the root, the minor 3rd and perfect 5th.
- * @see http://www.songtrellis.com/directory/1146/chordTypes/minorChordTypes/minorTriadMi
- * The three notes of the chord have the same weight
- *
- * @param chords Chords
- * @param root root note
- * @param contribution harmonic contribution
- * @param num_harmonics Number of harmonics that should contribute to the polyphonic profile (1 only considers the
- * fundamental harmonic).
- * @param slope Value of the slope of the exponential harmonic contribution to the polyphonic profile.
- * @return std::vector<double> Chords with contribution added to its minor triad.
- */
 std::vector<double> AddMinorTriad(const std::vector<double>& chords,
                                   const int root,
                                   const double contribution,
@@ -278,13 +174,6 @@ std::vector<double> AddMinorTriad(const std::vector<double>& chords,
   return AddContributionHarmonics(chords_third, fifth, contribution, num_harmonics, slope);
 }
 
-/**
- * @brief Resizes and interpolates the profiles to fit the pcp size.
- *
- * @param pcp_size Number of array elements used to represent a semitone times 12.
- * @param key_profile Key profile.
- * @return std::tuple<std::vector<double>, double, double> Tuple of (resized profile, mean, standard deviation).
- */
 std::tuple<std::vector<double>, double, double> ResizeProfileToPcpSize(const unsigned int pcp_size,
                                                                        const std::vector<double>& key_profile) {
   if (pcp_size < 12 || pcp_size % 12 != 0)
@@ -317,20 +206,6 @@ std::tuple<std::vector<double>, double, double> ResizeProfileToPcpSize(const uns
   return std::tuple<std::vector<double>, double, double>{ profile_do, mean_profile, std_profile };
 }
 
-/**
- * @brief Calculate correlation
- * Correlation coefficient with 'shift' on of the vectors is shifted in time,
- * and then the correlation is calculated, just like a cross-correlation.
- *
- * @param v1 Vector
- * @param mean1 Mean
- * @param std1 Standard deviation
- * @param v2 Vector
- * @param mean2 Mean
- * @param std2 Standard deviation
- * @param shift Amount the profile was shifted
- * @return double
- */
 double Correlation(const std::vector<double>& v1,
                    const double mean1,
                    const double std1,
@@ -341,6 +216,7 @@ double Correlation(const std::vector<double>& v1,
   double r = 0.0;
   int size = static_cast<int>(v1.size());
 
+  // TODO: Change this to a reduce
   for (int i = 0; i < size; i++) {
     int index = (i - shift) % size;
 
@@ -356,32 +232,12 @@ double Correlation(const std::vector<double>& v1,
   return r;
 }
 
-/**
- * @brief This algorithm computes key estimate given a pitch class profile (HPCP).
- *
- * @param pcp The input pitch class profile.
- * @param use_polphony Enables the use of polyphonic profiles to define key profiles (this includes the contributions
- * from triads as well as pitch harmonics).
- * @param use_three_chords Consider only the 3 main triad chords of the key (T, D, SD) to build the polyphonic profiles.
- * @param num_harmonics Number of harmonics that should contribute to the polyphonic profile (1 only considers the
- * fundamental harmonic).
- * @param slope Value of the slope of the exponential harmonic contribution to the polyphonic profile.
- * @param profile_type The type of polyphic profile to use for correlation calculation.
- * @param use_maj_min Use a third profile called 'majmin' for ambiguous tracks [4]. Only available for the edma, bgate
- * and braw profiles
- * @return KeyOutput A struct containing the following:
- *      key: Estimated key, from A to G.
- *      scale: Scale of the key (major or minor).
- *      strength: Strength of the estimated key.
- *      first_to_second_relative_strength: The relative strength difference between the best estimate and second best
- *       estimate of the key.
- */
 KeyOutput DetectKey(const std::vector<double>& pcp,
                     const bool use_polphony,
                     const bool use_three_chords,
                     const unsigned int num_harmonics,
                     const double slope,
-                    const PolyphicProfile profile_type,
+                    const std::string profile_type,
                     const bool use_maj_min) {
   unsigned int pcp_size = static_cast<unsigned int>(pcp.size());
   unsigned int n = pcp_size / 12;
@@ -624,7 +480,7 @@ KeyOutput DetectKey(const std::vector<double>& pcp,
   // In the case of Wei Chai algorithm, the scale is detected in a second step
   // In this point, always the major relative is detected, as it is the first
   // maximum
-  if (profile_type == PolyphicProfile::Weichai) {
+  if (profile_type == "Weichai") {
     if (scale == Scales::MINOR)
       throw std::runtime_error("Key: error in Wei Chai algorithm. Wei Chai algorithm does not support minor scales.");
 
--- src/core/key.h
+++ src/core/key.h
@@ -7,23 +7,6 @@
 namespace musher {
 namespace core {
 
-enum class PolyphicProfile {
-  Diatonic,
-  Krumhansl,
-  Temperley,
-  Weichai,
-  Tonictriad,
-  Temperley2005,
-  Thpcp,
-  Shaath,
-  Gomez,
-  Noland,
-  Edmm,
-  Bgate,
-  Braw,
-  Edma,
-};
-
 enum class Scales {
   MAJOR,
   MINOR,
@@ -37,26 +20,136 @@ struct KeyOutput {
   double first_to_second_relative_strength;
 };
 
+/**
+ * @brief Select a key profile given the type.
+ *
+ * About the Key Profiles:
+ * - 'Diatonic' - binary profile with diatonic notes of both modes. Could be useful for ambient music or diatonic music
+ *      which is not strictly 'tonal functional'
+ * - 'Tonic Triad' - just the notes of the major and minor chords. Exclusively for testing.
+ * - 'Krumhansl' - reference key profiles after cognitive experiments with users. They should work generally fine for
+ *      pop music.
+ * - 'Temperley' - key profiles extracted from corpus analysis of euroclassical music. Therefore, they perform best on
+ *      this repertoire (especially in minor).
+ * - 'Shaath' -  profiles based on Krumhansl's specifically tuned to popular and electronic music.
+ * - 'Noland' - profiles from Bach's 'Well Tempered Klavier'.
+ * - 'edma' - automatic profiles extracted from corpus analysis of electronic dance music [3]. They normally perform
+ *      better that Shaath's
+ * - 'edmm' - automatic profiles extracted from corpus analysis of electronic dance music and manually tweaked according
+ *      to heuristic observation. It will report major modes (which are poorly represented in EDM) as minor, but improve
+ *      performance otherwise [3].
+ * - 'braw' - profiles obtained by calculating the median profile for each mode from a subset of BeatPort dataset. There
+ *      is an extra profile obtained from ambiguous tracks that are reported as minor[4]
+ * - 'bgate' - same as braw but zeroing the 4 less relevant elements of each profile[4]
+ *
+ * References:
+ *  [1] E. Gómez, "Tonal Description of Polyphonic Audio for Music Content
+ *  Processing," INFORMS Journal on Computing, vol. 18, no. 3, pp. 294–304, 2006.
+ *  [2] D. Temperley, "What's key for key? The Krumhansl-Schmuckler key-finding algorithm reconsidered", Music
+ *  Perception vol. 17, no. 1,  pp. 65-100, 1999.
+ *  [3] Á. Faraldo, E. Gómez, S. Jordà, P.Herrera, "Key Estimation in Electronic"
+ *  Dance Music. Proceedings of the 38th International Conference on information"
+ *  Retrieval, pp. 335-347, 2016.
+ *  [4] Faraldo, Á., Jordà, S., & Herrera, P. (2017, June). A multi-profile method"
+ *  for key estimation in edm. In Audio Engineering Society Conference: 2017 AES"
+ *  International Conference on Semantic Audio. Audio Engineering Society.
+ *
+ * --essentia: https://github.com/MTG/essentia/blob/master/src/algorithms/tonal/key.cpp
+ *
+ * @param profile_type Key profile type.
+ * @return std::vector<std::vector<double>> Key profile
+ */
+std::vector<std::vector<double>> SelectKeyProfile(const std::string profile_type);
+
+/**
+ * @brief Add contribution harmonics to chords.
+ * Each note contribute to the different harmonics:
+ * 1.- first  harmonic  f   -> i
+ * 2.- second harmonic  2*f -> i
+ * 3.- third  harmonic  3*f -> i+7
+ * 4.- fourth harmonic  4*f -> i
+ * ..
+ * The contribution is weighted depending of the slope
+ *
+ * @param chords Chords
+ * @param pitch_class pitch class
+ * @param contribution harmonic contribution
+ * @param num_harmonics Number of harmonics that should contribute to the polyphonic profile (1 only considers the
+ * fundamental harmonic).
+ * @param slope Value of the slope of the exponential harmonic contribution to the polyphonic profile.
+ * @return std::vector<double> chords with added contribution harmonics
+ */
 std::vector<double> AddContributionHarmonics(const std::vector<double>& M_chords,
                                              const int pitch_class,
                                              const double contribution,
                                              const int num_harmonics,
                                              const double slope);
+
+/**
+ * @brief Adds the contribution of a chord with root note 'root' to its major triad.
+ * A major triad includes notes from three different classes of pitch: the root, the major 3rd and perfect 5th.
+ * This is the most relaxed, most consonant chord in all of harmony.
+ * @see http://www.songtrellis.com/directory/1146/chordTypes/majorChordTypes/majorTriad
+ * The three notes of the chord have the same weight
+ *
+ * @param chords Chords
+ * @param root root note
+ * @param contribution harmonic contribution
+ * @param num_harmonics Number of harmonics that should contribute to the polyphonic profile (1 only considers the
+ * fundamental harmonic).
+ * @param slope Value of the slope of the exponential harmonic contribution to the polyphonic profile.
+ * @return std::vector<double> Chords with contribution added to its major triad.
+ */
 std::vector<double> AddMajorTriad(const std::vector<double>& M_chords,
                                   const int root,
                                   const double contribution,
                                   const int num_harmonics,
                                   const double slope);
 
+/**
+ * @brief Adds the contribution of a chord with root note 'root' to its minor triad
+ * A minor triad includes notes from three different classes of pitch: the root, the minor 3rd and perfect 5th.
+ * @see http://www.songtrellis.com/directory/1146/chordTypes/minorChordTypes/minorTriadMi
+ * The three notes of the chord have the same weight
+ *
+ * @param chords Chords
+ * @param root root note
+ * @param contribution harmonic contribution
+ * @param num_harmonics Number of harmonics that should contribute to the polyphonic profile (1 only considers the
+ * fundamental harmonic).
+ * @param slope Value of the slope of the exponential harmonic contribution to the polyphonic profile.
+ * @return std::vector<double> Chords with contribution added to its minor triad.
+ */
 std::vector<double> AddMinorTriad(const std::vector<double>& M_chords,
                                   const int root,
                                   const double contribution,
                                   const int num_harmonics,
                                   const double slope);
 
-std::vector<std::vector<double>> SelectKeyProfile(const PolyphicProfile profile_type);
+/**
+ * @brief Resizes and interpolates the profiles to fit the pcp size.
+ *
+ * @param pcp_size Number of array elements used to represent a semitone times 12.
+ * @param key_profile Key profile.
+ * @return std::tuple<std::vector<double>, double, double> Tuple of (resized profile, mean, standard deviation).
+ */
 std::tuple<std::vector<double>, double, double> ResizeProfileToPcpSize(const unsigned int pcp_size,
                                                                        const std::vector<double>& key_profile);
+
+/**
+ * @brief Calculate correlation
+ * Correlation coefficient with 'shift' on of the vectors is shifted in time,
+ * and then the correlation is calculated, just like a cross-correlation.
+ *
+ * @param v1 Vector
+ * @param mean1 Mean
+ * @param std1 Standard deviation
+ * @param v2 Vector
+ * @param mean2 Mean
+ * @param std2 Standard deviation
+ * @param shift Amount the profile was shifted
+ * @return double
+ */
 double Correlation(const std::vector<double>& v1,
                    const double mean1,
                    const double std1,
@@ -64,12 +157,33 @@ double Correlation(const std::vector<double>& v1,
                    const double mean2,
                    const double std2,
                    const int shift);
+
+/**
+ * @brief This algorithm computes key estimate given a pitch class profile (HPCP).
+ *
+ * @param pcp The input pitch class profile.
+ * @param use_polphony Enables the use of polyphonic profiles to define key profiles (this includes the contributions
+ * from triads as well as pitch harmonics).
+ * @param use_three_chords Consider only the 3 main triad chords of the key (T, D, SD) to build the polyphonic profiles.
+ * @param num_harmonics Number of harmonics that should contribute to the polyphonic profile (1 only considers the
+ * fundamental harmonic).
+ * @param slope Value of the slope of the exponential harmonic contribution to the polyphonic profile.
+ * @param profile_type The type of polyphic profile to use for correlation calculation.
+ * @param use_maj_min Use a third profile called 'majmin' for ambiguous tracks [4]. Only available for the edma, bgate
+ * and braw profiles
+ * @return KeyOutput A struct containing the following:
+ *      key: Estimated key, from A to G.
+ *      scale: Scale of the key (major or minor).
+ *      strength: Strength of the estimated key.
+ *      first_to_second_relative_strength: The relative strength difference between the best estimate and second best
+ *       estimate of the key.
+ */
 KeyOutput DetectKey(const std::vector<double>& pcp,
                     const bool use_polphony = true,
                     const bool use_three_chords = true,
                     const unsigned int num_harmonics = 4,
                     const double slope = 0.6,
-                    const PolyphicProfile profile_type = PolyphicProfile::Bgate,
+                    const std::string profile_type = "Bgate",
                     const bool use_maj_min = false);
 
 }  // namespace core

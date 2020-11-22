50b3be61e850b6ca9e85ae4208e18deaf950eb6c
Joshua Maldonado <joshjm9915@gmail.com>
2020-11-22 14:41:28 -0500

    trying new build-wheels

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -16,17 +16,18 @@ yum install -y atlas-devel
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    # "${PYBIN}/pip" install -r /io/dev-requirements.txt
     "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
 done
 
-# Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
-    repair_wheel "$whl"
-done
+# # Bundle external shared libraries into the wheels
+# for whl in wheelhouse/*.whl; do
+#     repair_wheel "$whl"
+# done
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+    "${PYBIN}/pip" install musher --no-index -f /io/wheelhouse
+    # "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
+    (cd "$HOME"; "${PYBIN}/pytest" tests -v)
 done

dad0a88b9ac70dbec0cf07daf9f9e2a0cc5a5c3e
Joshua Maldonado <joshjm9915@gmail.com>
2020-11-24 15:45:57 -0500

    removed pypy support

--- tools/cibw-before-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Use an older numpy version of mac to avoid `Buggy Accelerate Backend` error.
-if [[ '$RUNNER_OS' == 'macOS' ]]; then 
-    pip install numpy==1.18.0;
-fi
-
-pip install -r dev_requirements.txt
